e9301a2a8b28af2ae34a4aa806de2382e74c7fe8
sc8668 <91195970+sc8668@users.noreply.github.com>
2022-06-08 15:53:58 +0800

    Add files via upload

---
+++ scripts/casf2016_screening2.py
@@ -0,0 +1,164 @@
+import numpy as np
+import torch as th
+from joblib import Parallel, delayed
+import pandas as pd
+import os, sys
+import pickle
+sys.path.append("/home/shenchao/resdocktest/deepdock2")
+from torch.utils.data import DataLoader
+from RTMScore.data.data import VSDataset
+from RTMScore.model.utils import collate, run_an_eval_epoch
+from RTMScore.model.model2 import RTMScore, DGLGraphTransformer #LigandNet, TargetNet
+import torch.multiprocessing
+torch.multiprocessing.set_sharing_strategy('file_system')
+args={}
+args["batch_size"] = 128
+args["aux_weight"] = 0.001
+args["dist_threhold"] = 5
+args['device'] = 'cuda' if th.cuda.is_available() else 'cpu'
+args['seeds'] = 126
+args["num_workers"] = 48
+args["model_path"] = "/home/shenchao/resdocktest/deepdock2/trained_models/rtmscore_model1.pth"
+args["cutoff"] = 10.0
+args["disttype"] = "min"  ##"min","ca","center"
+args["num_node_featsp"] = 41
+args["num_node_featsl"] = 41
+args["num_edge_featsp"] = 5
+args["num_edge_featsl"] = 10
+args["hidden_dim0"] = 128
+args["hidden_dim"] = 128 
+args["n_gaussians"] = 10
+args["dropout_rate"] = 0.10
+args["outprefix"] = "rtmscore1x5"
+
+def scoring(prot, lig, modpath,
+			cut=10.0,
+			explicit_H=False, 
+			use_chirality=True,
+			parallel=False,
+			**kwargs
+			):
+	"""
+	prot: The input protein file ('.pdb')
+	lig: The input ligand file ('.sdf|.mol2', multiple ligands are supported)
+	modpath: The path to store the pre-trained model
+	gen_pocket: whether to generate the pocket from the protein file.
+	reflig: The reference ligand to determine the pocket.
+	cutoff: The distance within the reference ligand to determine the pocket.
+	explicit_H: whether to use explicit hydrogen atoms to represent the molecules.
+	use_chirality: whether to adopt the information of chirality to represent the molecules.	
+	parallel: whether to generate the graphs in parallel. (This argument is suitable for the situations when there are lots of ligands/poses)
+	kwargs: other arguments related with model
+	"""
+	#try:
+	data = VSDataset(ligs=lig,
+					prot=prot,
+					cutoff=cut,		
+					explicit_H=explicit_H, 
+					use_chirality=use_chirality,
+					parallel=parallel)
+						
+	test_loader = DataLoader(dataset=data, 
+							batch_size=kwargs["batch_size"],
+							shuffle=False, 
+							num_workers=kwargs["num_workers"],
+							collate_fn=collate)
+	
+	ligmodel = DGLGraphTransformer(in_channels=kwargs["num_node_featsl"], 
+									edge_features=kwargs["num_edge_featsl"], 
+									num_hidden_channels=kwargs["hidden_dim0"],
+									activ_fn=th.nn.SiLU(),
+									transformer_residual=True,
+									num_attention_heads=4,
+									norm_to_apply='batch',
+									dropout_rate=0.15,
+									num_layers=6
+									)
+	
+	protmodel = DGLGraphTransformer(in_channels=kwargs["num_node_featsp"], 
+									edge_features=kwargs["num_edge_featsp"], 
+									num_hidden_channels=kwargs["hidden_dim0"],
+									activ_fn=th.nn.SiLU(),
+									transformer_residual=True,
+									num_attention_heads=4,
+									norm_to_apply='batch',
+									dropout_rate=0.15,
+									num_layers=6
+									)
+	
+	model = RTMScore(ligmodel, protmodel, 
+					in_channels=kwargs["hidden_dim0"], 
+					hidden_dim=kwargs["hidden_dim"], 
+					n_gaussians=kwargs["n_gaussians"], 
+					dropout_rate=kwargs["dropout_rate"], 
+					disttype=kwargs["disttype"], 
+					dist_threhold=kwargs["dist_threhold"]).to(kwargs['device'])
+	
+	checkpoint = th.load(modpath, map_location=th.device(kwargs['device']))
+	model.load_state_dict(checkpoint['model_state_dict']) 
+	preds = run_an_eval_epoch(model, test_loader, pred=True, dist_threhold=kwargs['dist_threhold'], device=kwargs['device'])	
+	return data.ids, preds
+	#except:
+	#	print("failed to scoring for {} and {}".format(prot, lig))
+	#	return None, None
+
+
+
+def score_compound(pdbid, ligid, prefix, **args):
+	return scoring(prot="/home/shenchao/pdbbind/%s/%s/%s_prot/%s_p_pocket_10.0.pdb"%(prefix, pdbid, pdbid, pdbid), 
+					lig="/home/shenchao/test/CASF-2016/decoys_screening/%s/%s_%s.sdf"%(pdbid, pdbid, ligid),
+					modpath=args["model_path"],
+					cut=args["cutoff"],
+					explicit_H=False, 
+					use_chirality=True,
+					parallel=True,
+					**args
+					)
+
+
+def score_compound2(pdbid, prefix, ligids, **args):
+	print("%s started....."%pdbid)
+	ids_list = []
+	scores_list = []
+	for ligid in ligids:
+		ids, scores = score_compound(pdbid, ligid, prefix, **args)
+		if ids is not None and scores is not None:
+			ids_list.extend(ids)
+			scores_list.extend(scores)
+	print("%s finished....."%pdbid)
+	return pdbid, [ids_list, scores_list]
+
+	
+ligids = [x for x in os.listdir('/home/shenchao/test/CASF-2016/coreset') if os.path.isdir("/home/shenchao/test/CASF-2016/coreset/%s"%(x))]
+pdbids = [x for x in os.listdir("/home/shenchao/test/CASF-2016/decoys_screening") if os.path.isdir("/home/shenchao/test/CASF-2016/decoys_screening/%s"%(x))]	
+pdbids1 = [x for x in os.listdir("/home/shenchao/pdbbind/v2020-refined") if os.path.isdir("/home/shenchao/pdbbind/v2020-refined/%s"%(x))]	
+pdbids2 = [x for x in os.listdir("/home/shenchao/pdbbind/v2020-other-PL") if os.path.isdir("/home/shenchao/pdbbind/v2020-other-PL/%s"%(x))]	
+
+ids1 = [pdbid for pdbid in pdbids if pdbid in pdbids1]
+ids2 = [pdbid for pdbid in pdbids if pdbid in pdbids2]
+if args['device'] == 'cpu':
+	results1 = Parallel(n_jobs=-1)(delayed(score_compound2)(pdbid, "v2020-refined", ligids, **args) for pdbid in ids1)
+	results2 = Parallel(n_jobs=-1)(delayed(score_compound2)(pdbid, "v2020-other-PL", ligids, **args) for pdbid in ids2)
+	results = results1 + results2
+else:
+	results = []
+	for pdbid in ids1:
+		results.append(score_compound2(pdbid, "v2020-refined", ligids, **args))
+	for pdbid in ids2:
+		results.append(score_compound2(pdbid, "v2020-other-PL", ligids, **args))
+
+outdir = "/home/shenchao/test/CASF-2016/power_screening/examples/%s"%args["outprefix"]
+os.system("mkdir -p %s"%outdir)
+for res in results:
+	#pdbid = res[0][0].split("_")[0]
+	pdbid = res[0]
+	df = pd.DataFrame(zip(*res[1]),columns=["#code_ligand_num","score"])
+	df["#code_ligand_num"] = df["#code_ligand_num"].str.split("-").apply(lambda x : x[0])
+	df.to_csv("%s/%s_score.dat"%(outdir, pdbid), index=False, sep="\t")
+
+with open("%s_screening.pkl"%args["outprefix"],"wb") as dbFile:
+	pickle.dump(results,dbFile)
+
+
+
+
---
+++ scripts/train_model.py
@@ -0,0 +1,129 @@
+import torch as th
+import numpy as np
+import dgl
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import sys
+sys.path.append("/home/shenchao/resdocktest/deepdock2")
+from RTMScore.data.data import PDBbindDataset
+from RTMScore.model.model2 import RTMScore, DGLGraphTransformer #LigandNet, TargetNet
+from RTMScore.model.utils import collate, EarlyStopping, set_random_seed, run_a_train_epoch, run_an_eval_epoch, mdn_loss_fn
+import torch.multiprocessing
+torch.multiprocessing.set_sharing_strategy('file_system')
+args={}
+args["num_epochs"] = 5000
+args["batch_size"] = 128
+args["aux_weight"] = 0.001
+args['patience'] = 70 
+args["num_workers"] = 10
+args["model_path"] = "xxx.pth"
+args['mode'] = "lower"
+args['lr'] = 3
+args['weight_decay'] = 5
+args['device'] = 'cuda' if th.cuda.is_available() else 'cpu'
+args['seeds'] = 126
+args["data_dir"] = "/home/shenchao/resdocktest"
+args["train_prefix"] = "v2020_train"
+#args["test1_prefix"] = "v2020_casf"
+#args["test2_prefix"] = "v2020_core"
+args["disttype"] = "min"  ##"ca","ce"
+args["valnum"] = 1500
+#args["rgcn_hidden_feats"] = [64, 64, 64]
+args["hidden_dim0"] = 128
+args["hidden_dim"] = 128 
+args["n_gaussians"] = 10
+args["dropout_rate"] = 0.10
+args["dist_threhold"] = 7.
+
+
+data = PDBbindDataset(ids="%s/%s_idsresx.npy"%(args["data_dir"], args["train_prefix"]),
+					  graphs="%s/%sresx.bin"%(args["data_dir"], args["train_prefix"])
+					  )
+
+train_inds, val_inds = data.train_and_test_split(valnum=args["valnum"], seed=args['seeds'])
+train_data = PDBbindDataset(ids=data.pdbids[train_inds],
+							graphs=np.array(data.graphs)[train_inds]
+							)
+val_data = PDBbindDataset(ids=data.pdbids[val_inds],
+							graphs=np.array(data.graphs)[val_inds]
+							)
+#test_data1 = PDBbindDataset(ids="%s/%s_idsresx.npy"%(args["data_dir"], args["test1_prefix"]),
+#					  graphs="%s/%sresx.bin"%(args["data_dir"], args["test1_prefix"])
+#					  )
+#test_data2 = PDBbindDataset(ids="%s/%s_idsresx.npy"%(args["data_dir"], args["test2_prefix"]),
+#					  graphs="%s/%sresx.bin"%(args["data_dir"], args["test2_prefix"])
+#					  )
+
+
+ligmodel = DGLGraphTransformer(in_channels=41, 
+								edge_features=10, 
+								num_hidden_channels=args["hidden_dim0"],
+								activ_fn=th.nn.SiLU(),
+								transformer_residual=True,
+								num_attention_heads=4,
+								norm_to_apply='batch',
+								dropout_rate=0.15,
+								num_layers=6
+								)
+
+protmodel = DGLGraphTransformer(in_channels=41, 
+									edge_features=5, 
+									num_hidden_channels=args["hidden_dim0"],
+									activ_fn=th.nn.SiLU(),
+									transformer_residual=True,
+									num_attention_heads=4,
+									norm_to_apply='batch',
+									dropout_rate=0.15,
+									num_layers=6
+									)
+
+model = RTMScore(ligmodel, protmodel, 
+				in_channels=args["hidden_dim0"], 
+				hidden_dim=args["hidden_dim"], 
+				n_gaussians=args["n_gaussians"], 
+				dropout_rate=args["dropout_rate"],
+				disttype=args["disttype"], 
+				dist_threhold=args["dist_threhold"]).to(args['device'])
+
+optimizer = th.optim.Adam(model.parameters(), lr=10**-args['lr'], weight_decay=10**-args['weight_decay'])
+
+
+
+train_loader = DataLoader(dataset=train_data, 
+							batch_size=args["batch_size"],
+							shuffle=True, 
+							num_workers=args["num_workers"],
+							collate_fn=collate)
+
+val_loader = DataLoader(dataset=val_data, 
+							batch_size=args["batch_size"],
+							shuffle=False, 
+							num_workers=args["num_workers"],
+							collate_fn=collate)
+
+
+stopper = EarlyStopping(patience=args['patience'], mode=args['mode'], filename=args["model_path"])
+
+set_random_seed(args["seeds"])
+for epoch in range(args["num_epochs"]):	
+	# Train
+	total_loss_train, mdn_loss_train, atom_loss_train, bond_loss_train = run_a_train_epoch(epoch, model, train_loader, optimizer, aux_weight=args["aux_weight"], device=args["device"])	
+	if np.isinf(mdn_loss_train) or np.isnan(mdn_loss_train): 
+		print('Inf ERROR')
+		break
+	# Validation and early stop
+	total_loss_val, mdn_loss_val, atom_loss_val, bond_loss_val = run_an_eval_epoch(model, val_loader, dist_threhold=args["dist_threhold"], aux_weight=args["aux_weight"], device=args["device"])
+	early_stop = stopper.step(total_loss_val, model)
+	print('epoch {:d}/{:d}, total_loss_val {:.4f}, mdn_loss_val {:.4f}, atom_loss_val {:.4f}, bond_loss_val {:.4f}, best validation {:.4f}'.format(epoch + 1, args['num_epochs'], total_loss_val, mdn_loss_val, atom_loss_val, bond_loss_val, stopper.best_score)) #+' validation result:', validation_result)
+	if early_stop:
+		break
+
+		
+stopper.load_checkpoint(model)
+total_loss_train, mdn_loss_train, atom_loss_train, bond_loss_train = run_an_eval_epoch(model, train_loader, dist_threhold=args["dist_threhold"], aux_weight=args["aux_weight"], device=args["device"])
+total_loss_val, mdn_loss_val, atom_loss_val, bond_loss_val = run_an_eval_epoch(model, val_loader, dist_threhold=args["dist_threhold"], aux_weight=args["aux_weight"], device=args["device"])		
+
+print("total_loss_train:%s, mdn_loss_train:%s, atom_loss_train:%s, bond_loss_train:%s"%(total_loss_train, mdn_loss_train, atom_loss_train, bond_loss_train))
+print("total_loss_val:%s, mdn_loss_val:%s, atom_loss_val:%s, bond_loss_val:%s"%(total_loss_val, mdn_loss_val, atom_loss_val, bond_loss_val))
+
+
